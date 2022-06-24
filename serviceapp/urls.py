38cf58e09a23d2847c4cb9b446160b54873714df
trivendra rahi <62616657+trakagreat@users.noreply.github.com>
2022-06-24 04:23:51 -0700

    iuyhw

--- serviceapp/urls.py
+++ serviceapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('service-form', views.ServiceFormView.as_view(), name='service_form_url'),
     path('service-form', views.PleaseLoginView.as_view(), name='login_first_url'),
     path('service-form/<slug:slug>/description', views.ServiceFormDesView.as_view(), name='service_form_des_url'),
-    path('service-detail/<slug:slug>', views.ServiceDetailView.as_view(), name='service-detail-page'),
+    path('<slug:slug>', views.ServiceDetailView.as_view(), name='service-detail-page'),
      path('image', views.ServiceImageUploadView.as_view(), name='service_image_upload_url'),
     path('test', views.TestView.as_view(), name='test'),
 ]
