729a3b8ad49e372e23e74cf08536cc7e218d0591
trivendra rahi <62616657+trakagreat@users.noreply.github.com>
2022-06-27 21:20:07 -0700

    share button

---
+++ serviceapp/static/serviceapp/share.js
@@ -0,0 +1,19 @@
+const shareData = {
+    title: 'Founde a service',
+    text: 'Check out this very Good service you can use',
+    url: "{% url 'service-detail-page' service.slug %}"
+  }
+  
+  const btn = document.querySelector('.share');
+  const resultPara = document.querySelector('.result');
+  
+  // Share must be triggered by "user activation"
+  btn.addEventListener('click', async () => {
+    try {
+      await navigator.share(shareData)
+      resultPara.textContent = 'MDN shared successfully'
+    } catch(err) {
+      resultPara.textContent = 'Error: ' + err
+    }
+  });
+  
