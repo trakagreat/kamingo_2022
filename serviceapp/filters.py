82be93d384a9cfd3a7b7ec20d49df53d85061749
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2022-09-08 17:57:54 +0530

    filter

--- serviceapp/filters.py
+++ serviceapp/filters.py
@@ -1,14 +1,25 @@
-from unicodedata import name
+
+
+from xml.dom.minidom import Attr
 import django_filters
 from .models import ServiceModel
+from django.forms.widgets import TextInput
 
 
 class ServiceFilter(django_filters.FilterSet):
 
-    address__city = django_filters.CharFilter(label='City',lookup_expr='icontains')
+   
+    address__city = django_filters.CharFilter(label='City',lookup_expr='icontains',widget=TextInput(attrs={
+        'placeholder':'City'
+    }))
+    address__pin_code = django_filters.CharFilter(widget=TextInput(attrs={
+        'placeholder':'Pincode'
+    }))
+
     class Meta:
         model = ServiceModel
         fields = ['category','address__city' , 'address__pin_code']
+
         url = 'serviceapp:front-page'
 
 
